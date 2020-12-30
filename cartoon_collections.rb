def143f3d9f3b6f00aec992fbefac29e73fc9299
C. L <amautacelt@gmail.com>
2020-12-30 03:17:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,8 @@ def greet_characters(array)
   # Print a custom greeting for each element
   
   characters_array = ["Hoggle", "Ludo", "Sir Didymus"]
-  
+  characters_array.each do |character|
+    
   
 end
 
