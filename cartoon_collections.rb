e0ac2823f6c0af0c27b71061fb0ad6f331551991
C. L <amautacelt@gmail.com>
2020-12-30 04:13:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def list_dwarves(array)
   # Print a numbered list of each element
   
   array.each_with_index do |character, index| 
-    p "#{index +1}:#{character}"
+    p "#{index +1}. #{character}"
   end
   
 end
